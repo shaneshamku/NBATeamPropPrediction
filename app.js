bc3e956ff5da8ddb2dea902db9720da60f52249b
shaneshamku <coolshane10@gmail.com>
2024-07-31 19:53:09 -0400

    Updated app.js - can query results and display on front end

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://coolshane10:JOr13Tcf5FO2YQ4T@prod-nba-predictions.a5lnoxs.mongodb.net/?retryWrites=true&w=majority&appName=prod-nba-predictions', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://coolshane10:JOr13Tcf5FO2YQ4T@prod-nba-predictions.a5lnoxs.mongodb.net/NBATeamPropPrediction?retryWrites=true&w=majority&appName=prod-nba-predictions', { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log('MongoDB connected successfully'))
+.catch(error => console.error('MongoDB connection error:', error));
+
 
 const gameSchema = new mongoose.Schema({
     team: String,
@@ -19,14 +22,34 @@ const gameSchema = new mongoose.Schema({
     prediction_adj: Number,
     prediction: Number,
     actual: Number,
-});
+}, { collection: 'predictions' });
 
 const Game = mongoose.model('Game', gameSchema);
 
+// Route for the root URL
+app.get('/', (req, res) => {
+  res.send('Welcome to the NBA Predictions API!');
+});
+
 app.get('/games', async (req, res) => {
-  const { season, month, day } = req.query;
-  const games = await Game.find({ season, month, day });
-  res.json(games);
+  try {
+    const { season, month, day } = req.query;
+    console.log('Received query parameters:', { season, month, day });
+
+    // Convert query parameters to integers
+    const seasonInt = parseInt(season, 10);
+    const monthInt = parseInt(month, 10);
+    const dayInt = parseInt(day, 10);
+    console.log('Converted query parameters:', { season: seasonInt, month: monthInt, day: dayInt });
+
+    const games = await Game.find({ season: seasonInt, month: monthInt, day: dayInt });
+    console.log('Games found:', games);
+    res.json(games);
+
+} catch (error) {
+    console.error('Error fetching games:', error);
+    res.status(500).json({ error: error.message });
+}
 });
 
 const PORT = process.env.PORT || 5000;
